c29917c7159f197511c3c1cec2896cc28007c0b2
bindu1206 <bindui1206@gmail.com>
2025-10-22 01:28:25 +0530

    feat: configure handlebars view engine and static assets

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const connectDB = require("./config/db");
+const path = require('path')
+const express = require("express")
+const dotenv = require("dotenv")
+const morgan = require('morgan')
+const { engine } = require('express-handlebars')
+const connectDB = require("./config/db")
 
 // Load environment variables
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: "./config/config.env" })
 
 // Connect to database
 connectDB();
 
 const app = express();
+
+//Using Morgan - HTTP request logger
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'))
+}
+
+//Handlebars
+app.engine('.hbs', engine({defaultLayout: 'main' , extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+//Static folder
+app.use(express.static(path.join( __dirname , 'public')));
 
 const PORT = process.env.PORT || 3000;
 
